d1f0a11911877f08b3428eb72be5e7e8a8782de4
jerry <jerry.john.jacob@live.com>
2014-07-09 07:44:15 +0530

    added delete_all command to collection

--- js/collections/items.js
+++ js/collections/items.js
@@ -1,5 +1,12 @@
 var Items = Backbone.Collection.extend({
   initialize: function(){
     
+  },
+
+  delete_all: function(){
+    var item;
+    while(item = this.first()) {
+      item.destroy();
+    }
   }
 });
